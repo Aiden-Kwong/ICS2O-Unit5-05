cfbc1d694858379a93f6edae85221977c4b1286a
AidenKwong <>
2021-06-03 13:37:54 +0000

    javascript touch-up

--- script.js
+++ script.js
@@ -10,9 +10,8 @@ function calculateArea () {
   // Check if length is a valid number, else display error message.
   if (!isNaN(length)) {
     length = length * length // Multiplies length by itself to get area.
-  document.getElementById('answer').innerHTML = 'The area is '+ length + '.' // Displays the answer.
-  } else { 
+    document.getElementById('answer').innerHTML = 'The area is ' + length + '.' // Displays the answer.
+  } else {
     document.getElementById('answer').innerHTML = 'Please enter a valid number!' // Displays error message.
   }
-  
 }
